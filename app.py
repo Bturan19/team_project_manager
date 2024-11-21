6dd61ca6972d95c694d18a0b407107269386de78
Bozkurt Turan Yiğit <turan.yigit@topkapidanismanlik.com.tr>
2024-11-21 14:02:31 +0300

    strftime

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ def run_app(name):
                 )
                 # Add vertical line for today's date
                 fig.add_vline(
-                    x=pd.to_datetime('today').normalize().to_pydatetime(),
+                    x=pd.to_datetime('today').normalize().to_pydatetime().strftime('%Y-%m-%d'),
                     line_width=2,
                     line_dash="dash",
                     line_color="red",
